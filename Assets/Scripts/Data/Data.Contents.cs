26a68860a2dbae675d5a4567fbe9ccf88cbee25a
Minwoo Kim <minwook1130@gmail.com>
2023-08-24 21:37:30 -0400

    Created Data script separately

---
+++ Assets/Scripts/Data/Data.Contents.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+#region Stat
+
+[Serializable]
+public class Stat
+{
+    public int level;
+    public int hp;
+    public int attack;
+}
+
+[Serializable]
+public class StatData : IDataLoader<int, Stat>
+{
+    public List<Stat> stats = new List<Stat>();
+
+    public Dictionary<int, Stat> LoadData()
+    {
+        Dictionary<int, Stat> dict = new Dictionary<int, Stat>();
+        foreach (Stat stat in stats)
+        {
+            dict.Add(stat.level, stat);
+        }
+
+        return dict;
+    }
+}
+
+#endregion
--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -3,30 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[Serializable]
-public class Stat
-{
-    public int level;
-    public int hp;
-    public int attack;
-}
-
-[Serializable]
-public class StatData : IDataLoader<int, Stat>
-{
-    public List<Stat> stats = new List<Stat>();
-
-    public Dictionary<int, Stat> LoadData()
-    {
-        Dictionary<int, Stat> dict = new Dictionary<int, Stat>();
-        foreach (Stat stat in stats)
-        {
-            dict.Add(stat.level, stat);
-        }
-
-        return dict;
-    }
-}
 
 public interface IDataLoader<Key, Value>
 {
@@ -35,13 +11,18 @@ public interface IDataLoader<Key, Value>
 
 public class DataManager
 {
+    // Data Containers
     public Dictionary<int, Stat> PlayerStats = new Dictionary<int, Stat>();
 
+
+
+    // Initialize any required data here in prior to the start of the game
     public void Init()
     {
         PlayerStats = LoadJson<StatData, int, Stat>("PlayerStat").LoadData();
     }
 
+    // Template method to load any Json data into a Data Container
     private T LoadJson<T, Key, Value>(string path) where T: IDataLoader<Key, Value>
     {
         TextAsset asset = GameManager.Resource.Load<TextAsset>($"Data/{path}");
